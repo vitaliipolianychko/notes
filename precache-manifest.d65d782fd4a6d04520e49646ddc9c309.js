92dadd2c5a54529c66a60c24036aed12cdca114e
vitaliipolianychko <vet.polyanychko@gmail.com>
2019-11-28 15:58:55 +0200

    Updates

---
+++ precache-manifest.d65d782fd4a6d04520e49646ddc9c309.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c57363e45b55aac4ad9e5ff346257d",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "97055e865c4c439c134b",
+    "url": "/notes/static/css/main.3f139bec.chunk.css"
+  },
+  {
+    "revision": "9c7bd5fcd44bc6aed6d7",
+    "url": "/notes/static/js/2.4d8d168c.chunk.js"
+  },
+  {
+    "revision": "97055e865c4c439c134b",
+    "url": "/notes/static/js/main.12e79131.chunk.js"
+  },
+  {
+    "revision": "4375e18f2464565398d5",
+    "url": "/notes/static/js/runtime-main.61cdf6c9.js"
+  },
+  {
+    "revision": "fd4e8664dba92f2caae95335d44709fa",
+    "url": "/notes/static/media/add_folder.fd4e8664.svg"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/notes/static/media/add_note.2927f8de.svg"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/notes/static/media/delete_note.650cdc57.svg"
+  },
+  {
+    "revision": "75b6fa6a62ebf5c22bf05895f05a113d",
+    "url": "/notes/static/media/edit.75b6fa6a.svg"
+  }
+]);
